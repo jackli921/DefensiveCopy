8a9e2b592e21355786db304cb55d204092aafe65
Jack Li <jack.li@digitalcni.com>
2024-06-18 11:51:34 -0400

    initial commit

---
+++ DefensiveCopy/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DefensiveCopy;
+
+// Math competition
+string[] a = new []{"Rick", "Sam", "Andre"};
+
+TopThree mathWinners = new TopThree(a, true);
+Console.WriteLine($"Math Competition Winners: {string.Join(", ", mathWinners.DisplayWinners())}");
+Console.WriteLine("");
+
+// Art competition
+a[0] = "Brett";
+a[1] = "Graham";
+a[2] = "Nick";
+
+TopThree artWinners = new TopThree(a, false);
+Console.WriteLine($"Art Competition Winners: {string.Join(", ", artWinners.DisplayWinners())}");
+Console.WriteLine("");
+
+Console.WriteLine($"Math Competition Winners (After argument object was modified): {string.Join(", ", mathWinners.DisplayWinners())}");
+Console.WriteLine("");
+
+string[] myWinners = mathWinners.DisplayWinners(); // returning an new memory address 
+
+myWinners[0] = "Hacker 1"; // modifying data in the new memory location; doesn't affect mathWinner's data
+myWinners[1] = "Hacker 2"; // modifying data in the new memory location; doesn't affect mathWinner's data
+myWinners[2] = "Hacker 3"; // modifying data in the new memory location; doesn't affect mathWinner's data
+
+Console.WriteLine(string.Join(",", myWinners)); 
+Console.WriteLine("");
+
+Console.WriteLine($"Math Competition Winners (After returning a copy of winners): {string.Join(", ", mathWinners.DisplayWinners())}");
---
+++ DefensiveCopy/TopThree.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+
+namespace DefensiveCopy;
+
+public class TopThree
+{
+    private string[] winners;
+    private bool isNational;
+
+    public TopThree(string[] winners, bool isNational)
+    {
+        this.winners = winners.ToArray(); // create defensive copy and store the memory address of the copy  
+        this.isNational = isNational;
+    }
+
+    public string[] DisplayWinners()
+    {
+        return winners.ToArray(); //create a defensive copy and return the memory address of newly created copy
+    }
+}
